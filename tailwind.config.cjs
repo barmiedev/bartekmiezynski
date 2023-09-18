b14318f8b0c6d6ca2b5b952f82354d577b84fc08
Bartłomiej Mieżyński <bartek@barmie.dev>
2023-09-18 19:59:42 +0200

    chore: tailwind configuration

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,21 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    container: {
+      screens: {
+        '2xl': '1400px',
+      },
+    },
+    extend: {
+      colors: {
+        'background': '#040202',
+        'text': '#ffffff',
+        'primary': '#841010',
+        'secondary': '#3f0808',
+        'accent': '#8a5c5c',
+        'tertiary': '#ffffff3d',
+      },
+    },
   },
   plugins: [],
 };
