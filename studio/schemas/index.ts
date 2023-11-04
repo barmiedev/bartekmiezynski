0a61d55dfc9acf208294a74907365c7841375331
Bartłomiej Mieżyński <bartek@barmie.dev>
2023-11-04 21:05:48 +0100

    :sparkles: added studio with sanity

---
+++ studio/schemas/index.ts
@@ -0,0 +1,6 @@
+import externalLink from './externalLink'
+import photo from './photo'
+import release from './release'
+import song from './song'
+
+export const schemaTypes = [release, photo, song, externalLink]
