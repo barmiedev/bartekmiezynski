960372651545ead818e4c0726723d7a31cb077e0
Bartłomiej Mieżyński <bartek@barmie.dev>
2023-11-04 21:11:40 +0100

    :wrench: added Sanity integration to Astro

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,11 +1,19 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sanity from 'astro-sanity';
 import netlify from '@astrojs/netlify/functions';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     tailwind(),
+    sanity({
+      projectId: import.meta.env.VITE_SANITY_PROJECT_ID,
+      dataset: import.meta.env.VITE_SANITY_DATASET,
+      apiVersion: '2021-03-25',
+      useCdn: true,
+      token: import.meta.env.VITE_SANITY_TOKEN,
+    }),
   ],
   output: 'server',
   adapter: netlify(),
