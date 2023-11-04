ee9c010c67173f988d8736b3ffa395eed2f89183
Bartłomiej Mieżyński <bartek@barmie.dev>
2023-11-04 22:03:29 +0100

    :package: changed sanity-astro package to official

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import sanity from 'astro-sanity';
 import netlify from '@astrojs/netlify/functions';
+import sanity from '@sanity/astro';
 
 // https://astro.build/config
 export default defineConfig({
