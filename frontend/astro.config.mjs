4496b8c062bea9b9e6f6a684aa6866952134d4dd
barmiedev <bartek@barmie.dev>
2024-02-04 20:22:42 +0100

    🔧 fixed build process

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,13 +1,13 @@
 import netlify from '@astrojs/netlify/functions';
 import tailwind from '@astrojs/tailwind';
-import sanity from '@sanity/astro';
+import { sanityIntegration } from '@sanity/astro';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     tailwind(),
-    sanity({
+    sanityIntegration({
       projectId: import.meta.env.VITE_SANITY_PROJECT_ID,
       dataset: import.meta.env.VITE_SANITY_DATASET,
       apiVersion: '2021-03-25',
