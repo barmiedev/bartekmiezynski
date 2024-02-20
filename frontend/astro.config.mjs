02144fd6f788e36d419b4bb56298c4d3367a777f
barmiedev <bartek@barmie.dev>
2024-02-20 23:50:00 +0100

    📦️ Added Astro Icons
    Mainly for social links

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,6 +1,7 @@
 import netlify from '@astrojs/netlify';
 import tailwind from '@astrojs/tailwind';
 import { sanityIntegration } from '@sanity/astro';
+import icon from 'astro-icon';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
@@ -13,6 +14,26 @@ export default defineConfig({
       apiVersion: '2021-03-25',
       useCdn: true,
       token: import.meta.env.VITE_SANITY_TOKEN,
+    }),
+    icon({
+      svgoOptions: {
+        multipass: true,
+        plugins: [
+          {
+            name: 'preset-default',
+            params: {
+              overrides: {
+                // customize default plugin options
+                inlineStyles: {
+                  onlyMatchedOnce: false,
+                },
+                // or disable plugins
+                removeDoctype: false,
+              },
+            },
+          },
+        ],
+      },
     }),
   ],
   output: 'server',
